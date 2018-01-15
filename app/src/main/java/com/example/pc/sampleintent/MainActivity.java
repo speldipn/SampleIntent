74bc1e313c716c668d6dd939068198ce499e5097
LAPTOP-IMLBTFBC\pc <speldipn@gmail.com>
2018-01-15 16:06:47 +0900

    Initial commit

---
+++ app/src/main/java/com/example/pc/sampleintent/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.pc.sampleintent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+  public static final int REQUEST_CODE_MENU = 101;
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    if(requestCode == REQUEST_CODE_MENU) {
+      Toast.makeText(getApplicationContext(),
+        "onACtivityResult 메소드 호출됨, 요청 코드 : " + requestCode +
+      ", 결과코드 :" + resultCode, Toast.LENGTH_LONG).show();
+
+      if(resultCode == RESULT_OK) {
+        String name = data.getExtras().getString("name");
+        Toast.makeText(getApplicationContext(), "응답으로 전달된 name : " + name, Toast.LENGTH_LONG).show();
+      }
+    }
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+
+  public void onButton1Clicked(View v) {
+    Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+    startActivityForResult(intent, REQUEST_CODE_MENU);
+  }
+}
